537725860a13a980a4c9e187d045236468aca3fc
Hiroyuki Ito <hageyahhoo@gmail.com>
2015-06-03 12:55:30 +0900

    Implemented partially.

--- src/test/java/com/thehiro/wdj/WeatherChecker.java
+++ src/test/java/com/thehiro/wdj/WeatherChecker.java
@@ -1,22 +1,29 @@
 package com.thehiro.wdj;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Check the weather via Yahoo!
+ * Check the weather via Yahoo!.
  * 
  * @author Hiroyuki Ito
  */
 public class WeatherChecker {
 
 	/**
-	 * 工事中
+	 * Check the weekly weather information of Tokyo.
 	 * 
 	 * @param args Nothing special.
 	 */
 	public static void main(String[] args) {
+		// TODO JUnit の dependency を pom.xml に追加する。
+
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.yahoo.co.jp");
+
+		driver.findElement(By.className("cbysC13")).click();
+		driver.findElement(By.linkText("東京")).click();
+		// TODO ここから先は CSS Selector の出番
 	}
 }
